0edafd42dee382ec3429c42dbd3fb8fab6a8e545
CyrilenBlu <lukepetzerint2@gmail.com>
2018-10-08 11:56:16 +0200

    Added index page and controller.

---
+++ src/main/java/blue/springframework/recipe/controllers/IndexController.java
@@ -0,0 +1,13 @@
+package blue.springframework.recipe.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class IndexController
+{
+    @RequestMapping({"","/","/index"})
+    public String getIndexPage()
+    {
+        return "index";
+    }
+
+}
