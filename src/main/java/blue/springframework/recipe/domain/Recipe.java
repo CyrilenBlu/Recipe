ebd683f8796c8392e991b8445d48f8098c6f3594
CyrilenBlu <lukepetzerint2@gmail.com>
2018-10-15 17:09:11 +0200

    Fixed null stack.

--- src/main/java/blue/springframework/recipe/domain/Recipe.java
+++ src/main/java/blue/springframework/recipe/domain/Recipe.java
@@ -1,11 +1,17 @@
 package blue.springframework.recipe.domain;
 
-import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 public class Recipe
 {
