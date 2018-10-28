d6016f2eb50fe0c91db4ea81edb2a38e86f4f811
BuildTools <unconfigured@null.spigotmc.org>
2018-10-28 15:08:46 +0200

    Added IngredientController j4 test

---
+++ src/test/java/blue/springframework/controllers/IngredientControllerTest.java
@@ -0,0 +1,50 @@
+package blue.springframework.controllers;
+
+import blue.springframework.domain.Recipe;
+import blue.springframework.services.RecipeService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+class IngredientControllerTest {
+
+    @Mock
+    RecipeService recipeService;
+
+    IngredientController controller;
+
+    MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+        controller = new IngredientController(recipeService);
+        mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    void listIngredients() throws Exception {
+        Recipe recipe = new Recipe();
+        recipe.setId(2L);
+
+        when(recipeService.findById(anyLong())).thenReturn(recipe);
+
+
+        mockMvc.perform(get("/recipe/1/ingredients"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("recipe/ingredient/list"))
+                .andExpect(model().attributeExists("recipe"));
+
+        verify(recipeService, times(0)).findCommandById(anyLong());
+
+    }
+}
