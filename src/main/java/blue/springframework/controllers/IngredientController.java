db8af877856c33db8127b76c1106c4d3eef2bfe7
BuildTools <unconfigured@null.spigotmc.org>
2018-10-28 14:17:31 +0200

    Added list ingredients.

---
+++ src/main/java/blue/springframework/controllers/IngredientController.java
@@ -0,0 +1,30 @@
+package blue.springframework.controllers;
+
+import blue.springframework.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@Slf4j
+public class IngredientController {
+    private final RecipeService recipeService;
+
+    public IngredientController(RecipeService recipeService) {
+        this.recipeService = recipeService;
+    }
+
+    @GetMapping
+    @RequestMapping("recipe/{id}/ingredients")
+    public String listIngredients(@PathVariable String id, Model model)
+    {
+        log.debug("Ingredient List pageId: " + id);
+        model.addAttribute("recipe", recipeService.findById(Long.valueOf(id)));
+        return "recipe/ingredient/list";
+    }
+
+
+}
