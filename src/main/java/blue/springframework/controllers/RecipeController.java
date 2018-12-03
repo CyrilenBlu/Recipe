7abece3970f44cc0cee6b695f12ee73405cabdc0
CyrilenBlu <lukepetzerint2@gmail.com>
2018-12-03 13:29:28 +0200

    Added 404 error page for recipe.

--- src/main/java/blue/springframework/controllers/RecipeController.java
+++ src/main/java/blue/springframework/controllers/RecipeController.java
@@ -1,14 +1,14 @@
 package blue.springframework.controllers;
 
 import blue.springframework.commands.RecipeCommand;
+import blue.springframework.exceptions.NotFoundException;
 import blue.springframework.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @Slf4j
@@ -61,4 +61,15 @@ public class RecipeController {
         return "redirect:/";
     }
 
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @ExceptionHandler(NotFoundException.class)
+    public ModelAndView handleNotFound()
+    {
+        log.error("Handling not found exception");
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("404error");
+        return modelAndView;
+
+    }
 }
