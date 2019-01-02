c76d897ccb7ee71775d8f6e00afc781ad6a5d888
BuildTools <unconfigured@null.spigotmc.org>
2019-01-02 14:42:44 +0200

    Database initialization per profile fix.

---
+++ src/main/java/blue/springframework/bootstrap/BootStrapMySQL.java
@@ -0,0 +1,93 @@
+package blue.springframework.bootstrap;
+
+import blue.springframework.domain.Category;
+import blue.springframework.domain.UnitOfMeasure;
+import blue.springframework.repositories.CategoryRepository;
+import blue.springframework.repositories.UnitOfMeasureRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Profile;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@Profile({"dev","prod"})
+public class BootStrapMySQL implements ApplicationListener<ContextRefreshedEvent> {
+    private final CategoryRepository categoryRepository;
+    private final UnitOfMeasureRepository unitOfMeasureRepository;
+
+    public BootStrapMySQL(CategoryRepository categoryRepository, UnitOfMeasureRepository unitOfMeasureRepository) {
+        this.categoryRepository = categoryRepository;
+        this.unitOfMeasureRepository = unitOfMeasureRepository;
+    }
+
+    @Override
+    public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+
+        if (categoryRepository.count() == 0L)
+        {
+            log.debug("Loading Categories");
+            loadCategories();
+        }
+        if (unitOfMeasureRepository.count() == 0L)
+        {
+            log.debug("Loading UOMs");
+            loadUOM();
+        }
+    }
+
+    private void loadCategories()
+    {
+        Category cat1 = new Category();
+        cat1.setDescription("American");
+        categoryRepository.save(cat1);
+
+        Category cat2 = new Category();
+        cat2.setDescription("Italian");
+        categoryRepository.save(cat2);
+
+        Category cat3 = new Category();
+        cat3.setDescription("Mexican");
+        categoryRepository.save(cat3);
+
+        Category cat4 = new Category();
+        cat4.setDescription("Fast Food");
+        categoryRepository.save(cat4);
+    }
+
+    private void loadUOM()
+    {
+        UnitOfMeasure uom1 = new UnitOfMeasure();
+        uom1.setDescription("Teaspoon");
+        unitOfMeasureRepository.save(uom1);
+
+        UnitOfMeasure uom2 = new UnitOfMeasure();
+        uom1.setDescription("Tablespoon");
+        unitOfMeasureRepository.save(uom2);
+
+        UnitOfMeasure uom3 = new UnitOfMeasure();
+        uom1.setDescription("Cup");
+        unitOfMeasureRepository.save(uom3);
+
+        UnitOfMeasure uom4 = new UnitOfMeasure();
+        uom1.setDescription("Pint");
+        unitOfMeasureRepository.save(uom4);
+
+        UnitOfMeasure uom5 = new UnitOfMeasure();
+        uom1.setDescription("Ounce");
+        unitOfMeasureRepository.save(uom5);
+
+        UnitOfMeasure uom6 = new UnitOfMeasure();
+        uom1.setDescription("Dash");
+        unitOfMeasureRepository.save(uom6);
+
+        UnitOfMeasure uom7 = new UnitOfMeasure();
+        uom1.setDescription("Each");
+        unitOfMeasureRepository.save(uom7);
+
+        UnitOfMeasure uom8 = new UnitOfMeasure();
+        uom1.setDescription("Clove");
+        unitOfMeasureRepository.save(uom8);
+    }
+}
